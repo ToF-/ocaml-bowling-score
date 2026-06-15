ca33e3f7f4b35a9bc9f4a1724fc71e0dc82edcf2
Christophe Thibaut <cthibauttof@gmail.com>
2026-06-15 18:38:43 +0200

    after ten frames rolls don't count

--- lib/bowling.ml
+++ lib/bowling.ml
@@ -2,11 +2,17 @@ let score rolls =
     let roll = Array.of_list rolls in
     let factor = Array.init (List.length rolls) (fun _ -> 1) in
     let rec collect_factors f i rolls = match rolls with
+    | (a::rest) when f >= 10 ->
+            factor.(i) <- factor.(i) - 1;
+            collect_factors f (i + 1) rest
     | [] -> ()
-    | (a::b::c::rest) when a = 10 -> factor.(i+1) <- factor.(i+1) + 1 ; factor.(i+2) <- factor.(i+2) + 1; collect_factors (f + 1) (i + 1) (b::c::rest)
+    | (a::b::c::rest) when a = 10 ->
+            factor.(i+1) <- factor.(i+1) + 1;
+            factor.(i+2) <- factor.(i+2) + 1;
+            collect_factors (f + 1) (i + 1) (b::c::rest)
     | (a::b::c::rest) when a + b = 10 -> factor.(i+2) <- factor.(i+2) + 1 ; collect_factors (f + 1) (i + 2) (c::rest)
-    | (a::b::rest) -> if f >= 10 then factor.(i) <- factor.(i) - 1 ; collect_factors (f + 1) (i + 2) rest
-    | (a::rest) -> if f >= 10 then factor.(i) <- factor.(i) - 1; collect_factors (f + 1) (i + 1) rest
+    | (a::b::rest) -> collect_factors (f + 1) (i + 2) rest
+    | [a] -> ()
     in
     let result = ref 0 in
     collect_factors 0 0 rolls ;
