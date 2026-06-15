8f36de0f79154d0219abece425ece0dcfd8e5e01
ToF- <cthibauttof@gmail.com>
2026-06-15 21:33:24 +0200

    refactoring

--- lib/bowling.ml
+++ lib/bowling.ml
@@ -1,17 +1,21 @@
 let score rolls =
     let roll = Array.of_list rolls in
     let factor = Array.init (List.length rolls) (fun _ -> 1) in
-    let rec collect_factors f i rolls = match rolls with
-    | (a::rest) when f >= 10 ->
-            factor.(i) <- factor.(i) - 1;
-            collect_factors f (i + 1) rest
-    | [] -> ()
+    let rec collect_factors frame index rolls = match rolls with
+    | [] ->
+            ()
+    | (a::rest) when frame >= 10 ->
+            factor.(index) <- factor.(index) - 1 ;
+            collect_factors frame (index + 1) rest
     | (a::b::c::rest) when a = 10 ->
-            factor.(i+1) <- factor.(i+1) + 1;
-            factor.(i+2) <- factor.(i+2) + 1;
-            collect_factors (f + 1) (i + 1) (b::c::rest)
-    | (a::b::c::rest) when a + b = 10 -> factor.(i+2) <- factor.(i+2) + 1 ; collect_factors (f + 1) (i + 2) (c::rest)
-    | (a::b::rest) -> collect_factors (f + 1) (i + 2) rest
+            factor.(index+1) <- factor.(index+1) + 1 ;
+            factor.(index+2) <- factor.(index+2) + 1 ;
+            collect_factors (frame + 1) (index + 1) (b::c::rest)
+    | (a::b::c::rest) when a + b = 10 ->
+            factor.(index+2) <- factor.(index+2) + 1 ;
+            collect_factors (frame + 1) (index + 2) (c::rest)
+    | (a::b::rest) ->
+            collect_factors (frame + 1) (index + 2) rest
     | [a] -> ()
     in
     let result = ref 0 in
