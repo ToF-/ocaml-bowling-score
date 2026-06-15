59a59173e69bfc9a6c4735df975ff40663643db3
ToF- <cthibauttof@gmail.com>
2026-06-15 15:32:38 +0200

    given a spare then next roll is added as bonus

--- lib/bowling.ml
+++ lib/bowling.ml
@@ -1,3 +1,12 @@
-let rec score = function
-| [] -> 0
-| l ->  List.hd l + (score (List.tl l))
+let score rolls =
+    let roll = Array.of_list rolls in
+    let factor = Array.init (List.length rolls) (fun _ -> 1) in
+    let rec collect_factors i rolls = match rolls with
+    | [] -> ()
+    | (a::b::c::rest) when a + b = 10 -> factor.(i+2) <- factor.(i+2) + 1 ; collect_factors (i + 2) (c::rest)
+    | (a::rest) -> collect_factors (i + 1) rest
+    in
+    let result = ref 0 in
+    collect_factors 0 rolls ;
+    Array.iter2 (fun r f -> result := !result + r * f) roll factor ;
+    !result
--- test/test_ocaml_bowling_score.ml
+++ test/test_ocaml_bowling_score.ml
@@ -9,6 +9,8 @@ let tests = "bowling score" >:::
         assert_equal ~printer: string_of_int 7 (score [7]));
     ("given several average rolls then score is the sum of these rolls" >:: fun _ ->
         assert_equal ~printer: string_of_int 10 (score [3;2;4;1]));
+    ("given a spare then the next roll is added as bonus" >:: fun _ ->
+        assert_equal ~printer: string_of_int 14 (score [4;6;2]));
     ]
 
 let _ = run_test_tt_main tests
