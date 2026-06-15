ac2b4f1dccbce1d62f83965e2fb4d5869e07d609
ToF- <cthibauttof@gmail.com>
2026-06-15 21:56:08 +0200

    refactor

--- lib/bowling.ml
+++ lib/bowling.ml
@@ -1,24 +1,27 @@
-let score rolls =
-    let roll = Array.of_list rolls in
-    let factor = Array.init (List.length rolls) (fun _ -> 1) in
-    let rec collect_factors frame index rolls = match rolls with
-    | [] ->
-            ()
-    | (a::rest) when frame >= 10 ->
-            factor.(index) <- factor.(index) - 1 ;
-            collect_factors frame (index + 1) rest
-    | (a::b::c::rest) when a = 10 ->
-            factor.(index+1) <- factor.(index+1) + 1 ;
-            factor.(index+2) <- factor.(index+2) + 1 ;
-            collect_factors (frame + 1) (index + 1) (b::c::rest)
-    | (a::b::c::rest) when a + b = 10 ->
-            factor.(index+2) <- factor.(index+2) + 1 ;
-            collect_factors (frame + 1) (index + 2) (c::rest)
-    | (a::b::rest) ->
-            collect_factors (frame + 1) (index + 2) rest
-    | [a] -> ()
-    in
-    let result = ref 0 in
-    collect_factors 0 0 rolls ;
-    Array.iter2 (fun r f -> result := !result + r * f) roll factor ;
-    !result
+let score roll_list =
+  let rolls = Array.of_list roll_list in
+  let factors = Array.init (Array.length rolls) (fun _ -> 1) in
+  let increment index = factors.(index) <- factors.(index) + 1 in
+  let decrement index = factors.(index) <- factors.(index) - 1 in
+  let rec collect_factors frame index rolls =
+    match rolls with
+    | [] -> ()
+    | a :: rest when frame >= 10 ->
+        decrement index;
+        collect_factors frame (succ index) rest
+    | a :: b :: c :: rest when a = 10 ->
+        increment (succ index);
+        increment (succ (succ index));
+        collect_factors (succ frame) (succ index) (b :: c :: rest)
+    | a :: b :: c :: rest when a + b = 10 ->
+        increment (succ (succ index));
+        collect_factors (succ frame) (succ (succ index)) (c :: rest)
+    | a :: b :: rest -> collect_factors (succ frame) (succ (succ index)) rest
+    | [ a ] -> ()
+  in
+  let result = ref 0 in
+  collect_factors 0 0 roll_list;
+  Array.iter2
+    (fun roll factors -> result := !result + (roll * factors))
+    rolls factors;
+  !result
--- test/test_ocaml_bowling_score.ml
+++ test/test_ocaml_bowling_score.ml
@@ -2,22 +2,26 @@ open OUnit2
 open Printf
 open Ocaml_bowling_score.Bowling
 
-let tests = "bowling score" >:::
-    [("given no rolls then score is zero" >:: fun _ ->
-        assert_equal ~printer: string_of_int 0 (score []));
-    ("given one roll then score is that roll" >:: fun _ ->
-        assert_equal ~printer: string_of_int 7 (score [7]));
-    ("given several average rolls then score is the sum of these rolls" >:: fun _ ->
-        assert_equal ~printer: string_of_int 10 (score [3;2;4;1]));
-    ("given a spare then the next roll is added as bonus" >:: fun _ ->
-        assert_equal ~printer: string_of_int 14 (score [4;6;2]));
-    ("given a strike then the two next rolls are added as bonus" >:: fun _ ->
-        assert_equal ~printer: string_of_int 26 (score [10;4;3;2]));
-    ("a strike is valid on a new frame" >:: fun _ ->
-        assert_equal ~printer: string_of_int 21 (score [0;10;4;2;1]));
-    ("after 10 frames rolls are not added, only bonus" >:: fun _ ->
-        assert_equal ~printer: string_of_int 300 (score [10;10;10;10;10;10;10;10;10;10;10;10]));
-    ]
+let tests =
+  "bowling score"
+  >::: [
+         ( "given no rolls then score is zero" >:: fun _ ->
+           assert_equal ~printer:string_of_int 0 (score []) );
+         ( "given one roll then score is that roll" >:: fun _ ->
+           assert_equal ~printer:string_of_int 7 (score [ 7 ]) );
+         ( "given several average rolls then score is the sum of these rolls"
+         >:: fun _ ->
+           assert_equal ~printer:string_of_int 10 (score [ 3; 2; 4; 1 ]) );
+         ( "given a spare then the next roll is added as bonus" >:: fun _ ->
+           assert_equal ~printer:string_of_int 14 (score [ 4; 6; 2 ]) );
+         ( "given a strike then the two next rolls are added as bonus"
+         >:: fun _ ->
+           assert_equal ~printer:string_of_int 26 (score [ 10; 4; 3; 2 ]) );
+         ( "a strike is valid on a new frame" >:: fun _ ->
+           assert_equal ~printer:string_of_int 21 (score [ 0; 10; 4; 2; 1 ]) );
+         ( "after 10 frames rolls are not added, only bonus" >:: fun _ ->
+           assert_equal ~printer:string_of_int 300
+             (score [ 10; 10; 10; 10; 10; 10; 10; 10; 10; 10; 10; 10 ]) );
+       ]
 
 let _ = run_test_tt_main tests
-
